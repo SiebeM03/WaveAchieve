f09fc8e2df059da8b195b5daf120c05e958b9ae8
Siebe Michiels <r0901813@student.thomasmore.be>
2024-01-10 03:03:42 +0100

    project setup

--- App.js
+++ App.js
@@ -1,12 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
+
+// Navigation
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import { Ionicons, FontAwesome5 } from '@expo/vector-icons';
+
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import React from 'react'
+import TodoScreen from './components/todo_screen'
+
+
+const client = new ApolloClient({
+  uri: process.env.GRAPHQL_ENDPOINT,
+  headers: {
+    'x-hasura-admin-secret': process.env.GRAPHQL_KEY
+  },
+  cache: new InMemoryCache()
+});
+
+const Tab = createBottomTabNavigator();
+const theme = {
+  colors: {
+    primary: 'deepskyblue',
+  },
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <ApolloProvider client={ client }>
+
+        <NavigationContainer>
+          <Tab.Navigator>
+            <Tab.Screen name="Home" component={ TodoScreen }/>
+            <Tab.Screen name="Test" component={ TodoScreen }/>
+          </Tab.Navigator>
+        </NavigationContainer>
+      </ApolloProvider>
   );
 }
 
